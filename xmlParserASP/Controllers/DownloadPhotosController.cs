05bee72b4b1c64f8c21a7d483a78b73dad425655
Sh-Maxim-N <sh.maxim.n@gmail.com>
2023-07-28 18:58:57 +0300

    XL parsing foto

--- xmlParserASP/Controllers/DownloadPhotosController.cs
+++ xmlParserASP/Controllers/DownloadPhotosController.cs
@@ -3,19 +3,20 @@ using System.Xml;
 using xmlParserASP.Models;
 using System.Drawing;
 using System.Drawing.Imaging;
+using ClosedXML.Excel;
 
 namespace xmlParserASP.Controllers
 {
     public class DownloadPhotosController : Controller
     {
-        public IActionResult Index() 
+        public IActionResult Index()
         {
-            return View(); 
+            return View();
         }
         public async Task<ActionResult> DownloadFromXml()
         {
             try
-            {               
+            {
                 var xmlDoc = new XmlDocument();
                 xmlDoc.Load(PathModel.Path);
 
@@ -37,7 +38,7 @@ namespace xmlParserASP.Controllers
                     foreach (XmlNode photoNode in photoNodes)
                     {
                         var photoUrl = photoNode.InnerText;
-                        
+
                         var modelValue = photoNode.ParentNode.SelectSingleNode(PathModel.ModelNode).InnerText;
                         //var modelValue = photoNode.ParentNode.Attributes["id"]?.Value;                       
 
@@ -135,6 +136,142 @@ namespace xmlParserASP.Controllers
             }
 
             return View("DownloadFromXml");
-        }       
+        }
+
+
+        public async Task<ActionResult> DownloadFromXL()
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    // Create a dictionary to keep track of the count of each model
+                    var modelCount = new Dictionary<string, int>();
+                    int totalPhotosDownloaded = 0;
+                    int totalPhotosResized = 0;
+                    int totalPhotoPassedExists = 0;
+
+                    string excelFilePath = PathModel.Path;
+                    string modelColumnName = PathModel.ModelXlColumn; // Replace with the name of the column containing model names
+                    string photoUrlColumnName = PathModel.PictureXlColumn; // Replace with the name of the column containing photo URLs
+
+                    using (var workbook = new XLWorkbook(excelFilePath))
+                    {
+                        var worksheet = workbook.Worksheet(PathModel.XlSheetNumber); // Sheet Number
+                        var firstRowUsed = worksheet.FirstRowUsed();
+                        var modelColumn = firstRowUsed.CellsUsed().FirstOrDefault(c => c.Value.ToString() == modelColumnName)?.WorksheetColumn();
+                        var photoUrlColumn = firstRowUsed.CellsUsed().FirstOrDefault(c => c.Value.ToString() == photoUrlColumnName)?.WorksheetColumn();
+
+                        if (modelColumn == null || photoUrlColumn == null)
+                        {
+                            ViewBag.Message = "Model and/or Photo URL column not found in the Excel file.";
+                            return View("Index");
+                        }
+
+                        var currentRow = firstRowUsed.RowUsed().RowBelow(); // Skip the header row
+
+                        while (!currentRow.Cell(modelColumn.ColumnNumber()).IsEmpty())
+                        {
+                            var modelValue = currentRow.Cell(modelColumn.ColumnNumber()).Value.ToString();
+                            var photoUrl = currentRow.Cell(photoUrlColumn.ColumnNumber()).Value.ToString();
+
+                            var originalFileName = Path.GetFileName(photoUrl);
+
+                            if (!modelCount.ContainsKey(modelValue))
+                            {
+                                // Add the model to the dictionary with an initial count of 0
+                                modelCount[modelValue] = 0;
+                                totalPhotoPassedExists++;
+                            }
+
+                            // Increment the count for the model
+                            modelCount[modelValue]++;
+
+                            // Get the count value for the model
+                            var count = modelCount[modelValue];
+
+                            // Get the alphabetic character based on the count (A, B, C, ...)
+                            var alphabeticCharacter = ((char)('A' + count - 1)).ToString();
+
+                            // Combine modelValue, alphabeticCharacter, "Feron", and originalFileName to form the new file name
+                            var imageName = $"{modelValue}-{alphabeticCharacter}-{PathModel.Supplier}_{originalFileName}";
+
+                            // Check if the file already exists in the destination folder
+                            var filePath = Path.Combine(PathModel.PhotoFolder, imageName);
+                            //if (System.IO.File.Exists(filePath))
+                            //{
+                            //    totalPhotoPassedExists++;
+                            //    continue; // Skip downloading if the file already exists
+                            //}
+
+                            // Download the photo and save it to the selected folder
+                            using (var response = await client.GetAsync(photoUrl))
+                            {
+                                if (response.IsSuccessStatusCode)
+                                {
+                                    var photoFilePath = Path.Combine(PathModel.PhotoFolder, imageName);
+
+                                    using (var photoStream = await response.Content.ReadAsStreamAsync())
+                                    {
+                                        // Load the image from the stream
+                                        using (var image = Image.FromStream(photoStream))
+                                        {
+                                            photoStream.Seek(0, SeekOrigin.Begin);
+                                            // Check if the image exceeds the maximum size
+                                            if (image.Width > 1000 || image.Height > 1000)
+                                            {
+                                                // Calculate the new dimensions while maintaining the aspect ratio
+                                                int newWidth, newHeight;
+                                                if (image.Width > image.Height)
+                                                {
+                                                    newWidth = 1000;
+                                                    newHeight = (int)((float)image.Height / image.Width * newWidth);
+                                                }
+                                                else
+                                                {
+                                                    newHeight = 1000;
+                                                    newWidth = (int)((float)image.Width / image.Height * newHeight);
+                                                }
+
+                                                // Create a new bitmap with the resized dimensions
+                                                using (var resizedImage = new Bitmap(image, newWidth, newHeight))
+                                                {
+                                                    // Save the resized image to the file
+                                                    resizedImage.Save(photoFilePath, ImageFormat.Jpeg);
+                                                    totalPhotosResized++;
+                                                }
+                                            }
+                                            else
+                                            {
+                                                // Save the original image to the file
+                                                using (var fileStream = new FileStream(photoFilePath, FileMode.Create))
+                                                {
+                                                    photoStream.Seek(0, SeekOrigin.Begin);
+                                                    await photoStream.CopyToAsync(fileStream);
+                                                }
+                                            }
+                                            totalPhotosDownloaded++;
+                                        }
+                                    }
+                                }
+                            }
+
+                            // Move to the next row
+                            currentRow = currentRow.RowBelow();
+                        }
+                    }
+
+                    ViewBag.Message = $"Total photos downloaded: {totalPhotosDownloaded}. Total photos resized: {totalPhotosResized}. Photos passed because exists {totalPhotoPassedExists}";
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur during the process
+                ViewBag.Message = "An error occurred: " + ex.Message;
+            }
+
+            return View("DownloadFromXml");
+        }
+
     }
 }
--- xmlParserASP/Models/PathModel.cs
+++ xmlParserASP/Models/PathModel.cs
@@ -8,7 +8,7 @@ public enum Language
 }
 public static class PathModel
 {
-    public static string? Path { get; } = @"D:\Downloads\promua_2405ru.xml"; //work
+    //public static string? Path { get; } = @"D:\Downloads\promua_2405ru.xml"; //work
     //public static string? Path { get; } = @"https://horoz-electric.com.ua/products_feed.xml?hash_tag=143483e1e045e0b718c4c5f63cc41ea5&sales_notes=&product_ids=&label_ids=&exclude_fields=&html_description=0&yandex_cpa=&process_presence_sure=&languages=ru&group_ids=";
     //public static string? Path { get; } = @"D:\Downloads\Telegram Desktop\promua_2405ua.xml"; //home
 
@@ -16,6 +16,7 @@ public static class PathModel
     //public static string? Path { get;} = @"https://b2b.allegro-opt.com.ua/allegro-807a960d080cdbaeea3aec7f5cd8dede.xml"; // Videx, Allegro
     //public static string? Path { get; } = @"https://amperok.com.ua/index.php?route=extension/feed/unixml/violux_full"; // Violux, Amperok 
     //public static string? Path { get; } = @"https://b2b.vestum.ua/data/yml_catalog_rozetka.xml"; // Vestum
+    public static string? Path { get; } = @"D:\Downloads\Novodvor.xlsx";
 
 
 
@@ -28,14 +29,20 @@ public static class PathModel
     //public static string ProductNode = "offer";  // Horoz
 
     public static string ModelNode = "model";
+    public static string ModelXlColumn = "Артикул";
+
+    public static int XlSheetNumber = 2;
+
 
 
     public static string PictureNode = "image";
+    public static string PictureXlColumn = "URL";
+
     public static string imageNameInCatImg = "";
     
 
     public static string PhotoFolder = @"D:\Downloads\Photo\";
-    public static string Supplier = "Feron";
+    public static string Supplier = "Novodvorsky";
 
 
     public static string QuantityNode = "quantity";
