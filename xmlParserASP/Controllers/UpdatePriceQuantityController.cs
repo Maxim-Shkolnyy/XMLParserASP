0913b0d85a5394aa6b926571fb3c3833074f78c5
Shkolnyi Maxim <@foxstudent102139>
2023-09-10 19:49:14 +0300

    MODEL

---
+++ xmlParserASP/Controllers/UpdatePriceQuantityController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using xmlParserASP.Presistant;
+using xmlParserASP.Entities;
+
+namespace xmlParserASP.Controllers
+{
+    public class UpdatePriceQuantityController : Controller
+    {
+        private readonly MyDBContext _db;
+        private readonly SupplierXmlSetting _setting;
+        public UpdatePriceQuantityController(MyDBContext db, SupplierXmlSetting setting)
+        {
+            _db = db;
+            _setting = setting;
+        }
+        public IActionResult Index()
+        {
+            //var currentProduct = _db.Products.Where(m=>m.ProductId == _supplier.SupplierId && )
+            var settingList = _db.SupplierXmlSettings.ToList();
+            ViewBag.settingViewBag = settingList;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdatePriceQuantity()
+        {
+            
+
+            return View();
+        }
+    }
+}
---
+++ xmlParserASP/Models/PriceQuantityViewModel.cs
@@ -0,0 +1,13 @@
+﻿using xmlParserASP.Entities;
+
+namespace xmlParserASP.Models
+{
+    public class PriceQuantityViewModel
+    {
+        public double? Price { get; set; }
+        public string? Quantity { get; set; }
+        public string? SupplierId { get; set; }
+        public string? Model { get; set; }
+        public List<SupplierXmlSetting> SupplierXmlSettings { get; set; }
+    }
+}
