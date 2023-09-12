a0fc976a62a12d23972b3380113ed2bbe5e2c337
Sh-Maxim-N <sh.maxim.n@gmail.com>
2023-09-12 17:34:46 +0300

    5

--- xmlParserASP/Models/PriceQuantityViewModel.cs
+++ xmlParserASP/Models/PriceQuantityViewModel.cs
@@ -12,5 +12,5 @@ namespace xmlParserASP.Models
         public List<SupplierXmlSetting>? SupplierXmlSettings { get; set; }
         [Required(ErrorMessage = "Select a supplier")]
         public int SupplierXmlSettingId { get; set; }
-    }
+    } 
 }
