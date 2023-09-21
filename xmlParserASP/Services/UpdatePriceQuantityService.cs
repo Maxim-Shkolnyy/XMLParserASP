e534d0c0df011130c9723eb016e03f3235105f17
foxstudent102139 <sh.maxim.n@gmail.com>
2023-09-21 19:37:16 +0300

    5

--- xmlParserASP/Services/UpdatePriceQuantityService.cs
+++ xmlParserASP/Services/UpdatePriceQuantityService.cs
@@ -1,13 +1,32 @@
 ﻿using xmlParserASP.Controllers;
 using xmlParserASP.Entities;
+using xmlParserASP.Presistant;
 
 namespace xmlParserASP.Services
 {
     public class UpdatePriceQuantityService
     {
-        public string UpdatePrice(List <int> settingsId)
-        {
+        private readonly SupplierXmlSetting _supplierXmlSetting;
+        private readonly MyDBContext _dbContext;
 
+        public UpdatePriceQuantityService(SupplierXmlSetting supplierXmlSetting, MyDBContext myDBContext)
+        {
+            _supplierXmlSetting = supplierXmlSetting;
+            _dbContext = myDBContext;
+        }
+        public string UpdatePrice(List<int> settingsId)
+        {
+            if (settingsId == null)
+            {
+                return new string ("setting ID was not passed");
+            }
+
+            foreach (int id in settingsId)
+            {
+                var currentPrice = _supplierXmlSetting.ModelNode.
+            }
+
+            
 
             string uodateResult = string.Empty;
 
@@ -16,7 +35,15 @@ namespace xmlParserASP.Services
 
         public string UpdateQuantity(List<int> settingsId)
         {
+            if (settingsId == null)
+            {
+                return new string("setting ID was not passed");
+            }
 
+            foreach (int id in settingsId)
+            {
+
+            }
 
             string uodateResult = string.Empty;
 
