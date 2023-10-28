4447f7dc71f7522d4bab7ace8e088fb041d1f89b
foxstudent102139 <sh.maxim.n@gmail.com>
2023-10-28 13:18:53 +0300

    4

--- xmlParserASP/Services/UpdateMainXml.cs
+++ xmlParserASP/Services/UpdateMainXml.cs
@@ -1,4 +1,6 @@
-﻿using xmlParserASP.Entities.Gamma;
+﻿using Newtonsoft.Json;
+using xmlParserASP.Entities.Gamma;
+using xmlParserASP.Entities.TestGamma;
 using xmlParserASP.Presistant;
 
 namespace xmlParserASP.Services;
@@ -12,21 +14,41 @@ public class UpdateMainXml
         _dbContextGamma = gammaContext;
     }
 
-    public void UpdateGammaXml()
+    public string UpdateGammaXml()
     {
         var gammaAllProducts = _dbContextGamma.OcProducts.Where(p => !p.Sku.StartsWith("4") & !p.Sku.StartsWith("5")).ToList();
 
 
-        var AllProducts = _dbContextGamma.OcProducts.Where(p => !p.Sku.StartsWith("4") & !p.Sku.StartsWith("5")).
-            Join(_dbContextGamma.OcProductDescriptions.Where(m => m.LanguageId == 4)
-                .Select(n => new
-                {
-                    n.ProductId,
-                    n.Description
-                });
+        var AllProducts = _dbContextGamma.OcProducts.Where(p => !p.Sku.StartsWith("4") && !p.Sku.StartsWith("5")).
+        Join(_dbContextGamma.OcProductDescriptions.Where(p => p.LanguageId == 4), o => o.ProductId, i => i.ProductId, (o, i) => new
+        {
+            o.ProductId,
+            o.Sku,
+            i.Name,
 
+        });
 
+        string str = JsonConvert.SerializeObject(AllProducts);
 
+        return str;
+
+        var maxProd = _dbContextGamma.OcProducts.Where(n => n.Sku.StartsWith("4")).
+            Join(_dbContextGamma.OcProductDescriptions, o => o.ProductId, i => i.ProductId, (o, i) => new
+            {
+                o.Sku,
+                i.Name
+            });
+
+        // (o,i) => new { p.ProductId   }
+        // int fun(OCProduct o, OCProduct i)
+        // {
+        //      return p.ProductId;
+        // }
+
+        // LINQ to Objects 
+        // produts.Where(p => File.ReadAllLines().Contains(p.Name))
+
+        
 
 
         var skuPriceQty = gammaAllProducts.Select(p => new
@@ -37,7 +59,7 @@ public class UpdateMainXml
             p.Quantity
         });
 
-        var gammaNames = _dbContextGamma.OcProductDescriptions.Where(n => n.LanguageId ==4).Select(t => new
+        var gammaNames = _dbContextGamma.OcProductDescriptions.Where(n => n.LanguageId == 4).Select(t => new
         {
             t.ProductId,
             t.Name
