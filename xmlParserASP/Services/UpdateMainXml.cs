ea0a70ad0045b2155bd37ae4a4d24c8ece881075
Sh-Maxim-N <sh.maxim.n@gmail.com>
2023-11-03 16:12:35 +0200

    ftp-ok

--- xmlParserASP/Services/UpdateMainXml.cs
+++ xmlParserASP/Services/UpdateMainXml.cs
@@ -1,6 +1,4 @@
-﻿using Newtonsoft.Json;
-using xmlParserASP.Entities.Gamma;
-using xmlParserASP.Entities.TestGamma;
+﻿using xmlParserASP.Entities.Gamma;
 using xmlParserASP.Presistant;
 using xmlParserASP.Entities;
 using System.Xml.Serialization;
@@ -16,8 +14,9 @@ public class UpdateMainXml
     private readonly string _ftpHost = "zi391919.ftp.tools";
     private readonly string _ftpUser = "zi391919_victor";
     private readonly string _ftpPass = "6C8z94TFhn";
-    private readonly string _remoteFilePath = @"/image/catalog/xml/gamma/old1s.xml"; //\image\catalog\xml\exchange\max.xml
-    private readonly string _localFilePath = @"D:\Downloads\andr.txt";  // "/db_backups"
+    private readonly string _localFilePath = @"D:\ftp\";  // "/db_backups"
+    private readonly string _ftpDir = "/exchange/";
+    private readonly int deletionDays = 3;
 
     public UpdateMainXml(GammaContext gammaContext)
     {
@@ -27,7 +26,7 @@ public class UpdateMainXml
     public string UpdateGammaXml()
     {
         var query = from product in _dbContextGamma.OcProducts
-                    join prodName in _dbContextGamma.OcProductDescriptions on product.ProductId equals prodName.ProductId
+                    join prodName in _dbContextGamma.OcProductDescriptions.Where(p => p.LanguageId == 4) on product.ProductId equals prodName.ProductId
                     join prodCat in _dbContextGamma.OcProductToCategories on product.ProductId equals prodCat.ProductId
 
                     select new ProductToXml
@@ -43,147 +42,74 @@ public class UpdateMainXml
 
         var xmlSerializer = new XmlSerializer(typeof(List<ProductToXml>));
 
-        using (MemoryStream stream = new())
+        var localFile = Path.Combine(_localFilePath, "products1.xml");
+
+        using (FileStream stream = new(localFile, FileMode.Create))
         {
             xmlSerializer.Serialize(stream, query.ToList());
-            stream.Seek(0, SeekOrigin.Begin);
-
-            using (FtpClient client = new FtpClient(_ftpHost, _ftpUser, _ftpPass))
-            {
-                client.Connect();
-
-                if (client.IsConnected)
-                {
-                    // Upload the file
-                    client.UploadFile(_localFilePath, _remoteFilePath);
-
-                    Console.WriteLine("File uploaded successfully.");
-
-                    client.Disconnect();
-                }
-                else
-                {
-                    Console.WriteLine("Failed to connect to the FTP server.");
-                }
-            }
-
-
-            //FtpWebRequest request = (FtpWebRequest)WebRequest.Create(new Uri($"ftp://{_ftpHost}/image/catalog/xml/exchange/max.xml"));
-
-            //request.Credentials = new NetworkCredential(_ftpUser, _ftpPass);
-            //request.Method = WebRequestMethods.Ftp.UploadFile;
-
-            //using (Stream fileStream = File.OpenRead(@"D:\Downloads\andr.txt"))
-
-            //using (Stream ftpStream = request.GetResponse().GetResponseStream())
-            //{
-            //    fileStream.CopyTo(ftpStream);
-            //}
-            //FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-            //Console.WriteLine($"Upload File Complete, status {response.StatusDescription}");
-            //response.Close();
-
-
-
-
-
+            //stream.Seek(0, SeekOrigin.Begin);
         }
+
+        FTPUpload();
 
         string str = "";
         return str;
 
-
-
-        //var gammaAllProducts = _dbContextGamma.OcProducts.Where(p => !p.Sku.StartsWith("4") & !p.Sku.StartsWith("5")).ToList();
-
-
-        //var AllProducts = _dbContextGamma.OcProducts.Where(p => !p.Sku.StartsWith("4") && !p.Sku.StartsWith("5")).
-        //Join(_dbContextGamma.OcProductDescriptions.Where(p => p.LanguageId == 4),
-        //    o => o.ProductId, i => i.ProductId, (o, i) => new
-        //    {
-        //        o.ProductId,
-        //        o.Sku,
-        //        i.Name,
-
-        //    });
-
-
-        //var newQuery = _dbContextGamma.OcProducts.Where(p => !p.Sku.StartsWith("4") && !p.Sku.StartsWith("5")).
-        //    Join(_dbContextGamma.OcProductDescriptions.Where(p => p.LanguageId == 4),
-        //        product => product.ProductId, prodDesc => prodDesc.ProductId, (product, prodDesc) => new
-        //        {
-        //            product.ProductId,
-        //            product.Sku,
-        //            product.Price,
-        //            product.Quantity,
-        //            prodDesc.Name,
-
-        //        }).
-        //    Join(_dbContextGamma.OcProductToCategories, joined => joined.ProductId, prCat => prCat.ProductId, (joined, prCat) => new
-        //    {
-        //        joined.ProductId,
-        //        joined.Sku,
-        //        joined.Price,
-        //        joined.Quantity,
-        //        joined.Name,
-        //        prCat.CategoryId
-
-        //    }).
-        //    Select(result => result);
-
-
-        //string str = JsonConvert.SerializeObject(AllProducts);
-
-
-
-        //var maxProd = _dbContextGamma.OcProducts.Where(n => n.Sku.StartsWith("4")).
-        //    Join(_dbContextGamma.OcProductDescriptions, o => o.ProductId, i => i.ProductId, (o, i) => new
-        //    {
-        //        o.Sku,
-        //        i.Name
-        //    });
     }
 
-    static void FTPUpload()
+    private void FTPUpload()
     {
+        int attempts = 0;
         try
         {
-            FtpTrace.AddListener(new ConsoleTraceListener());
+            FtpClient client = new FtpClient(_ftpHost);
+            client.Credentials = new NetworkCredential(_ftpUser, _ftpPass);
 
-            FtpClient client = new FtpClient(ftpServer);
-            client.Credentials = new NetworkCredential(ftpLogin, ftpPassword);
-            client.RetryAttempts = 3;
+
+            Trace.TraceInformation("Attempting to open an FTP connection.");
+
             client.Connect();
 
-            if (!string.IsNullOrEmpty(ftpDir) && ftpDir != "/")
+            if (!string.IsNullOrEmpty(_ftpDir) && _ftpDir != "/")
             {
-                if (!client.DirectoryExists(ftpDir))
+                if (!client.DirectoryExists(_ftpDir))
                 {
-                    client.CreateDirectory(ftpDir);
+                    client.CreateDirectory(_ftpDir);
                 }
             }
 
-            foreach (FtpListItem item in client.GetListing(ftpDir))
+            foreach (FtpListItem item in client.GetListing(_ftpDir))
             {
-                if (item.Type == FtpFileSystemObjectType.File)
+                if (item.Type == FtpObjectType.File)
                 {
                     DateTime time = client.GetModifiedTime(item.FullName);
 
-                    if (time < DateTime.Now.AddDays(-DeletionDays))
+                    if (time < DateTime.Now.AddDays(-deletionDays))
                         client.DeleteFile(item.FullName);
                 }
             }
 
-            string[] localFiles = Directory.GetFiles(backupDir);
+            string[] localFiles = Directory.GetFiles(_localFilePath);
 
-            client.UploadFiles(localFiles, ftpDir, FtpExists.Skip, true, FtpVerify.Retry);
+            var oo = client.UploadFiles(localFiles, _ftpDir, FtpRemoteExists.Overwrite, true, FtpVerify.Retry);
 
             client.Disconnect();
-
+            attempts = 0;
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex.ToString());
+            attempts++;
+
+            if (attempts < 3)
+            {
+                Thread.Sleep(3000);
+                FTPUpload();
+            }
+            else
+            {
+                Console.WriteLine($"3 unsuccessful attempts to connect to ftp server: {ex.ToString()}");
+            }
+
         }
     }
 
