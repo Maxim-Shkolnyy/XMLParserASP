b6153c2e8a278934d499b4d7e2bd14a153cebcca
Sh-Maxim-N <sh.maxim.n@gmail.com>
2024-02-21 19:12:28 +0200

    1

---
+++ xmlParserASP/Controllers/BaseController.cs
@@ -0,0 +1,104 @@
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using xmlParserASP.Presistant;
+
+namespace xmlParserASP.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly GammaContext _db;
+        public BaseController(GammaContext db)
+        {
+                _db = db;
+        }
+        protected async Task<IActionResult> ExportToExcel<T>(IQueryable<T> data)
+        {
+            var modelName = typeof(T).Name;
+            var time = DateTime.Now;
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Max");
+                var currentRow = 1;
+
+                var properties = typeof(T).GetProperties();
+
+                var headerRow = worksheet.Row(currentRow);
+                for (int i = 0; i < properties.Length; i++)
+                {
+                    headerRow.Cell(i + 1).Value = properties[i].Name;
+                }
+                currentRow++;
+
+                foreach (var item in data)
+                {
+                    for (int i = 0; i < properties.Length; i++)
+                    {
+                        var value = properties[i].GetValue(item);
+                        worksheet.Cell(currentRow, i + 1).Value = value?.ToString();
+                    }
+                    currentRow++;
+                }
+
+                using (var stream = new MemoryStream())
+                {
+                    workbook.SaveAs(stream);
+
+                    var content = stream.ToArray();
+
+                    // Повернути файл Excel з масивом байтів та типом MIME для відображення в браузері
+                    return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", $"{modelName}_{time}.xlsx");
+                }
+            }
+        }
+
+
+        protected async Task<IActionResult> ImportFromExcel<T>(Stream stream, Func<string, T> mapper) where T : class
+        {
+            var entityType = typeof(T);
+
+            using (var workbook = new XLWorkbook(stream))
+            {
+                var worksheet = workbook.Worksheet(1);
+                var rowCount = worksheet.RowsUsed().Count();
+
+                for (int i = 2; i <= rowCount; i++)
+                {
+                    var entity = mapper(worksheet.Cell(i, 1).Value.ToString());
+                    if (entity != null)
+                    {
+                        var keyPropertyValue = worksheet.Cell(i, 1).Value.ToString(); // Assuming the first cell contains the key property value
+                        var existingEntity = await _db.FindAsync<T>(keyPropertyValue);
+
+
+                        // Find the existing entity by its key property value
+
+                        if (existingEntity != null)
+                        {
+                            var properties = entityType.GetProperties();
+                            for (int j = 0; j < properties.Length; j++)
+                            {
+                                var value = worksheet.Cell(i, j + 1).Value.ToString();
+                                var property = properties[j];
+                                // Convert value to the appropriate type if necessary
+                                if (property.PropertyType.IsEnum)
+                                {
+                                    property.SetValue(existingEntity, Enum.Parse(property.PropertyType, value));
+                                }
+                                else
+                                {
+                                    property.SetValue(existingEntity, Convert.ChangeType(value, property.PropertyType));
+                                }
+                            }
+                        }
+                    }
+                }
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
--- xmlParserASP/Controllers/ProductsManualSetPricesController.cs
+++ xmlParserASP/Controllers/ProductsManualSetPricesController.cs
@@ -1,15 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClosedXML.Excel;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.IO;
 using xmlParserASP.Entities.Gamma;
 using xmlParserASP.Presistant;
 
 namespace xmlParserASP.Controllers;
 
-public class ProductsManualSetPricesController : Controller
+public class ProductsManualSetPricesController : BaseController
 {
     private readonly GammaContext _context;
 
-    public ProductsManualSetPricesController(GammaContext context)
+    public ProductsManualSetPricesController(GammaContext context) : base(context)
     {
         _context = context;
     }
@@ -17,12 +19,77 @@ public class ProductsManualSetPricesController : Controller
     // GET: ProductsManualSetPrices
     public async Task<IActionResult> Index()
     {
-        return _context.ProductsManualSetPrices != null ? 
+        return _context.ProductsManualSetPrices != null ?
             View(await _context.ProductsManualSetPrices.ToListAsync()) :
             Problem("Entity set 'GammaContext.ProductsManualSetPrices'  is null.");
     }
 
-    // GET: ProductsManualSetPrices/Details/5
+
+    public async Task<IActionResult> ExportToExcel()
+    {
+        var prices = await _context.ProductsManualSetPrices.ToListAsync();
+        return await ExportToExcel(prices.AsQueryable());
+    }
+
+    // Import action
+    [HttpPost]
+
+    public async Task<IActionResult> ImportFromExcel(IFormFile file)
+    {
+        // Перевіряємо, чи файл був переданий
+        if (file == null || file.Length == 0)
+        {
+            ModelState.AddModelError("File", "Please select a file");
+            return View();
+        }
+
+        using (var workbook = new XLWorkbook(file.OpenReadStream()))
+        {
+            var worksheet = workbook.Worksheet(1);
+            var lastRow = worksheet.LastRowUsed().RowNumber();
+
+            // Виведення інформації про файл у вікно відладки
+            Console.WriteLine($"File Name: {file.FileName}");
+            Console.WriteLine($"Number of Rows: {lastRow}");
+
+            // Ітерація по кожному рядку у листі Excel
+            for (int i = 2; i <= lastRow; i++) // Починаємо з 2, оскільки 1-й рядок зазвичай містить заголовки
+            {
+                var excelRow = worksheet.Row(i);
+
+                // Викликаємо метод базового контролера для обробки рядка Excel
+                // В якості аргументів передаємо дані рядка та функцію мапування (MapExcelRowToEntity)
+                await base.ProcessExcelRow(excelRow, MapExcelRowToEntity);
+            }
+        }
+
+    }
+
+    private MmProductsManualSetPrice MapExcelRowToEntity(string excelRow)
+    {
+        var columns = excelRow.Split(';'); // Припустимо, що дані розділені символом ";"
+
+        if (columns.Length != 5) // Перевірка, чи кількість стовпців відповідає очікуваній структурі
+        {
+            // Якщо кількість стовпців не відповідає очікуваній, повертаємо null, означаючи, що рядок не може бути коректно мапований
+            return null;
+        }
+
+        // Створюємо новий об'єкт сутності і заповнюємо його поля з даних рядка
+        var entity = new MmProductsManualSetPrice
+        {
+            Id = int.Parse(columns[0]), // Перший стовпець мапуємо на Id
+            Sku = columns[1], // Другий стовпець мапуємо на Sku
+            SetInStockPrice = decimal.Parse(columns[2]), // Третій стовпець мапуємо на SetInStockPrice
+            DateStart = DateTime.Parse(columns[3]), // Четвертий стовпець мапуємо на DateStart
+            DateEnd = string.IsNullOrEmpty(columns[4]) ? (DateTime?)null : DateTime.Parse(columns[4]) // П'ятий стовпець мапуємо на DateEnd, з урахуванням можливого значення null
+        };
+
+        return entity;
+    }
+
+
+   // GET: ProductsManualSetPrices/Details/5
     public async Task<IActionResult> Details(int? id)
     {
         if (id == null || _context.ProductsManualSetPrices == null)
@@ -145,7 +212,7 @@ public class ProductsManualSetPricesController : Controller
         {
             _context.ProductsManualSetPrices.Remove(productsManualSetPrice);
         }
-            
+
         await _context.SaveChangesAsync();
         return RedirectToAction(nameof(Index));
     }
