a01e7235951586edd4a37d287f2378263057e9b3
Maxim-Shkolnyy <105427529+Maxim-Shkolnyy@users.noreply.github.com>
2024-05-29 23:36:44 +0300

    JWT

--- xmlParserASP/Program.cs
+++ xmlParserASP/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
 using xmlParserASP.Controllers;
 using xmlParserASP.Entities.Gamma;
 using xmlParserASP.Entities.Users;
@@ -8,6 +12,7 @@ using xmlParserASP.Presistant;
 using xmlParserASP.Services;
 using xmlParserASP.Services.UpdateServices;
 using xmlParserASP.Services.UpdateServices.XmlToGammaUpload_OLD;
+using System.Text;
 
 namespace xmlParserASP;
 
@@ -18,15 +23,73 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Configuration.AddUserSecrets<Program>();
-        
+
         builder.Services.AddDbContext<GammaContext>(options => options.UseMySQL(builder.Configuration.GetConnectionString("GammaConnection")));
         builder.Services.AddAntiforgery(options => { });
 
-        builder.Services.AddAuthorization();
-        builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme);
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = IdentityConstants.BearerScheme;
+            options.DefaultChallengeScheme = IdentityConstants.BearerScheme;
+        })
+        .AddJwtBearer(IdentityConstants.BearerScheme, options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            };
+        });
+
+        // Configure Authorization
+        builder.Services.AddAuthorization(options =>
+        {
+            // Add policies for User and Admin roles
+            options.AddPolicy("RequireUserRole", policy => policy.RequireRole("User"));
+            options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+        });
+
+        // Alternatively, you can use AddAuthorizationBuilder
+        builder.Services.AddAuthorizationBuilder()
+            .AddPolicy("RequireUserRole", policy => policy.RequireRole("User"))
+            .AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+
         builder.Services.AddIdentity<User, IdentityRole>()
             .AddEntityFrameworkStores<GammaContext>()
             .AddDefaultTokenProviders();
+
+        builder.Services.Configure<IdentityOptions>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequiredLength = 6;
+            options.Password.RequiredUniqueChars = 1;
+
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.AllowedForNewUsers = true;
+
+            options.User.AllowedUserNameCharacters =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+            options.User.RequireUniqueEmail = false;
+        });
+
+        builder.Services.ConfigureApplicationCookie(options =>
+        {
+            options.Cookie.HttpOnly = true;
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+            options.LoginPath = "/Identity/Account/Login";
+            options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            options.SlidingExpiration = true;
+        });
 
         builder.Services.AddControllersWithViews();
         builder.Services.AddScoped<MmSupplierXmlSetting>();
@@ -61,7 +124,7 @@ public class Program
 
         app.UseAuthentication();
         app.UseAuthorization();
- 
+
 
         app.MapControllerRoute(
             name: "default",
